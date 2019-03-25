670c01d7259ef38a183719e2c862515983a20052
Krystian Pietroń <krystianpietr@gmail.com>
2019-03-25 16:14:45 +0100

    add foto name and contact to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { FotoComponent } from './foto/foto.component';
+import { MyNameComponent } from './my-name/my-name.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotoComponent,
+    MyNameComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
